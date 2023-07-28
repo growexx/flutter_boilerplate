98fba0507481b9af7afe944694e2ce46dd178023
syed-growexxer <syed.raza@growexx.com>
2023-07-28 11:10:27 +0530

    Toast Added

--- lib/app_manager/helper/show_toast.dart
+++ lib/app_manager/helper/show_toast.dart
@@ -1,12 +1,12 @@
 
 import 'package:flutter/foundation.dart';
-// import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void showToast(String msg) {
   try {
-    // Fluttertoast.showToast(
-    //     msg: msg,
-    // );
+    Fluttertoast.showToast(
+        msg: msg,
+    );
   } catch(e) {
     if (kDebugMode) {
       print(e);
