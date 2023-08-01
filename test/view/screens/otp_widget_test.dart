a2fb9907877b2a3efb5fb54edb951d03704ee89b
aniruddha-growexxer <aniruddha.baidya@growexx.com>
2023-08-01 12:10:40 +0530

    test: 💍 OTP widget test case fixed

--- test/view/screens/otp_widget_test.dart
+++ test/view/screens/otp_widget_test.dart
@@ -6,6 +6,7 @@ import 'package:flutter_boilerplate/view_model/otp_view_model.dart';
 import 'package:flutter_boilerplate/view_model/social_signin_view_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import '../../util/common_initial_activity.dart';
 import '../../util/testing_material_app.dart';
 import 'otp_widget_test.mocks.dart';
@@ -23,30 +24,31 @@ import 'otp_widget_test.mocks.dart';
   MockSpec<UserRepository>(
       as: #MockUserRepository, onMissingStub: OnMissingStub.returnDefault),
 ])
-
 void main() async {
   await commonInitialActivity();
   TestWidgetsFlutterBinding.ensureInitialized();
 
   group("OTP Widget test", () {
     testWidgets('OTP widget', (WidgetTester tester) async {
-
       MockOTPViewModel mockOTPViewModel = MockOTPViewModel();
-      MockSocialSignInViewModel mockSocialSignInViewModel = MockSocialSignInViewModel();
+      MockSocialSignInViewModel mockSocialSignInViewModel =
+          MockSocialSignInViewModel();
       MockUserRepository mockUserRepository = MockUserRepository();
-
-      Widget widget = testingWidget(child: OTPWidget(googleSignInViewModel: mockSocialSignInViewModel,
-      userRepository: mockUserRepository,
-      viewModel: mockOTPViewModel,
-      key: const Key("12"),));
-
+      when(mockOTPViewModel.phoneC)
+          .thenAnswer((realInvocation) => TextEditingController());
+      Widget widget = testingWidget(
+          child: OTPWidget(
+        googleSignInViewModel: mockSocialSignInViewModel,
+        userRepository: mockUserRepository,
+        viewModel: mockOTPViewModel,
+        key: const Key("12"),
+      ));
 
       await tester.pumpWidget(widget);
 
       expect(find.byKey(const Key("send_otp")), findsOneWidget);
       expect(find.byKey(const Key("add_your_phone_number")), findsOneWidget);
       expect(find.byKey(const Key("phone")), findsOneWidget);
-
     });
   });
 }
