f70404d946fe750e40d8879c3221a9132303acd8
aniruddha-growexxer <aniruddha.baidya@growexx.com>
2023-08-09 12:30:57 +0530

    refactor: 💡 removed code smells

--- test/view/screens/navigation_screen_test.dart
+++ test/view/screens/navigation_screen_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_boilerplate/view/screens/screens.dart';
 import 'package:flutter_test/flutter_test.dart';
 
