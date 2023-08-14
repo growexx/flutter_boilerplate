0c54f56f813a75375e936fdc22d41cd4cb216017
Bhargav Joshi <bhargav.joshi@growexx.com>
2023-08-14 11:37:35 +0530

    Instagram auth in mobile app done

--- lib/view/screens/signin/instagram_signin_screen.dart
+++ lib/view/screens/signin/instagram_signin_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/authentication/user_repository.dart';
 import 'package:flutter_boilerplate/view_model/instagram_signin_view_model.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../../app_manager/helper/navigation/navigation_helper.dart';
+import '../../../authentication/user.dart';
+import '../navigation_screen.dart';
 
 class InstagramSigninScreen extends StatelessWidget {
   static const String name = "instagram-signin-screen";
@@ -19,9 +25,59 @@ class InstagramSigninScreen extends StatelessWidget {
               style: Theme.of(context).textTheme.headlineSmall,
             ),
           ),
-          body: WebViewWidget(controller: viewModel.getController()),
+          body: WebViewWidget(controller: getController(context, viewModel)),
         );
       },
     );
+  }
+
+  WebViewController getController(
+      BuildContext context, InstagramSigninViewModel viewModel) {
+    final userRepository = UserRepository();
+    return WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.transparent)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            print('Progressing $progress');
+          },
+          onPageStarted: (String url) {
+            print('page started $url');
+          },
+          onPageFinished: (String url) {
+            print('page finished $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            print('erorr ${error.description}');
+          },
+          onNavigationRequest: (request) async {
+            if (request.url.startsWith(viewModel.redirectUri)) {
+              print(
+                  '----------------------------------------------------${request.url}');
+              viewModel.getAuthorizationCode(request.url);
+              await viewModel.getTokenAndUserID().then((bool isDone) {
+                if (isDone) {
+                  viewModel.getUserProfile().then((User user) {
+                    userRepository.updateUserData(user).then((value) {
+                      NavigationHelper.pushNamed(
+                          context, NavigationScreen.name);
+                    });
+                    print('${user.firstName} logged in!');
+                  });
+                }
+              });
+              return NavigationDecision.prevent;
+            }
+            if (request.url.startsWith(
+                'https://www.instagram.com/accounts/manage_access/')) {
+              context.pop();
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(viewModel.url));
   }
 }
--- lib/view_model/instagram_signin_view_model.dart
+++ lib/view_model/instagram_signin_view_model.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import '../app_manager/constant/environment.dart';
 import 'package:http/http.dart' as http;
 
+import '../authentication/user.dart';
+
 class InstagramSigninViewModel extends ChangeNotifier {
   final url =
-      'https://api.instagram.com/oauth/authorize?client_id=${dotenv.env[Environment.appId]}&redirect_uri=${dotenv.env[Environment.firebaseRedirectURI]}&scope=user_profile,user_media&response_type=code';
+      'https://api.instagram.com/oauth/authorize?client_id=${dotenv.env[Environment.instagramAppID]}&redirect_uri=${dotenv.env[Environment.firebaseRedirectURI]}&scope=user_profile&response_type=code';
 
   final String redirectUri = dotenv.env[Environment.firebaseRedirectURI]!;
   final String instagramAppId = dotenv.env[Environment.instagramAppID]!;
@@ -16,48 +17,13 @@ class InstagramSigninViewModel extends ChangeNotifier {
       dotenv.env[Environment.instagramAppSecretKey]!;
 
   String? authorizationCode;
-  String? userID;
+  int? userID;
   String? accessToken;
 
-  WebViewController getController() {
-    return WebViewController()
-      ..setBackgroundColor(Colors.transparent)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            print('Progressing');
-          },
-          onPageStarted: (String url) {
-            print('page started $url');
-          },
-          onPageFinished: (String url) {
-            print('page finished $url');
-          },
-          onWebResourceError: (WebResourceError error) {
-            print('erorr $error');
-          },
-          onUrlChange: (change) async {
-            if (change.url!.contains(redirectUri)) {
-              getAuthorizationCode(change.url!);
-              await getTokenAndUserID().then((bool isDone) {
-                if (isDone) {
-                  getUserProfile().then((String? username) {
-                    print('$username logged in!');
-                  });
-                }
-              });
-            }
-          },
-          onNavigationRequest: (request) {
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse(url));
-  }
-
   void getAuthorizationCode(String url) {
-    authorizationCode = url.replaceAll('$url&code=', '').replaceAll('#_', '');
+    authorizationCode = url.replaceAll('$redirectUri?code=', '');
+    print(
+        '----------------------------------------------------------------authCode $authorizationCode');
   }
 
   Future<bool> getTokenAndUserID() async {
@@ -69,17 +35,23 @@ class InstagramSigninViewModel extends ChangeNotifier {
       'code': authorizationCode,
       'grant_type': 'authorization_code'
     });
+    print(
+        '------------------------------------------response ${response.body}');
     accessToken = json.decode(response.body)['access_token'];
     userID = json.decode(response.body)['user_id'];
     return (accessToken != null && userID != null) ? true : false;
   }
 
-  Future<String?> getUserProfile() async {
+  Future<User> getUserProfile() async {
     final List<String> userFields = ['id', 'username'];
     final fields = userFields.join(',');
     final responseNode = await http.get(Uri.parse(
         'https://graph.instagram.com/$userID?fields=$fields&access_token=$accessToken'));
+    print(
+        '----------------------------------------------------final response${responseNode.body}');
     final username = json.decode(responseNode.body)['username'];
-    return username;
+    final id = json.decode(responseNode.body)['id'];
+    final User user = User(id: id, firstName: username, lastName: '');
+    return user;
   }
 }
