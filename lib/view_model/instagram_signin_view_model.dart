b1d70809d7e6e8d32110a649ae040aff26079151
Bhargav Joshi <bhargav.joshi@growexx.com>
2023-08-14 19:55:28 +0530

    Instagram auth test cases added

--- lib/view_model/instagram_signin_view_model.dart
+++ lib/view_model/instagram_signin_view_model.dart
@@ -19,6 +19,7 @@ class InstagramSigninViewModel extends ChangeNotifier {
   String? authorizationCode;
   int? userID;
   String? accessToken;
+  http.Client client = http.Client();
 
   void getAuthorizationCode(String url) {
     authorizationCode = url.replaceAll('$redirectUri?code=', '');
@@ -26,7 +27,7 @@ class InstagramSigninViewModel extends ChangeNotifier {
 
   Future<bool> getTokenAndUserID() async {
     var url = Uri.parse('https://api.instagram.com/oauth/access_token');
-    final response = await http.post(url, body: {
+    final response = await client.post(url, body: {
       'client_id': instagramAppId,
       'redirect_uri': redirectUri,
       'client_secret': instagramAppSecretKey,
@@ -41,7 +42,7 @@ class InstagramSigninViewModel extends ChangeNotifier {
   Future<User> getUserProfile() async {
     final List<String> userFields = ['id', 'username'];
     final fields = userFields.join(',');
-    final responseNode = await http.get(Uri.parse(
+    final responseNode = await client.get(Uri.parse(
         'https://graph.instagram.com/$userID?fields=$fields&access_token=$accessToken'));
     final username = json.decode(responseNode.body)['username'];
     final id = json.decode(responseNode.body)['id'];
