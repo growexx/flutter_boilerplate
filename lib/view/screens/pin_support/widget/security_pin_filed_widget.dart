af1c26e8465102607df5cc30210c5156da20ef79
jaiminm-growexxer <jaimin.modi@growexx.com>
2023-08-16 14:38:21 +0530

    [complete] Biometrics - Added Create PIN Screen and Validated as well.

--- lib/view/screens/pin_support/widget/security_pin_filed_widget.dart
+++ lib/view/screens/pin_support/widget/security_pin_filed_widget.dart
@@ -1,10 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/app_manager/component/alert_dialog/alert_dialog_view.dart';
 import 'package:flutter_boilerplate/app_manager/helper/navigation/navigation_helper.dart';
 import 'package:flutter_boilerplate/view/screens/dashboard/dashboard_screen.dart';
 import 'package:flutter_boilerplate/view_model/security_pin_view_model.dart';
 import 'package:flutter_screen_lock/flutter_screen_lock.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:local_auth/local_auth.dart';
 
@@ -31,6 +33,52 @@ class _SecurityPinFieldWidgetState extends State<SecurityPinFieldWidget> {
                     return Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
+                        TextButton(
+                          key: const Key("create_pin"),
+                          onPressed: () {
+                            screenLockCreate(
+                                customizedButtonChild: !kIsWeb
+                                    ? const Icon(Icons.fingerprint)
+                                    : const SizedBox(height: 0, width: 0),
+                                customizedButtonTap: () async =>
+                                    await localAuth(context),
+                                title: const Text(
+                                  "create_security_pin",
+                                  style: TextStyle(color: Colors.white),
+                                ).tr(),
+                                cancelButton: const Text(
+                                  "cancel",
+                                  style: TextStyle(color: Colors.white),
+                                ).tr(),
+                                context: context,
+                                canCancel: true,
+                                onConfirmed: (String value) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialogView(
+                                          key: const Key("security_pin_dialog"),
+                                          title: 'security_pin'.tr(),
+                                          message:
+                                              'security_pin_description_done'.tr(),
+                                          popButtonTitle: '',
+                                          successButtonTitle: 'okay'.tr(),
+                                          onPressFunction: () async {
+                                            context.pop();
+                                          });
+                                    },
+                                  );
+                                });
+                          },
+                          style: TextButton.styleFrom(
+                            minimumSize: const Size.fromHeight(50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                          child: const Text("create_security_pin").tr(),
+                        ),
+                        const SizedBox(height: 20),
                         TextButton(
                           key: const Key("verify_pin"),
                           onPressed: () {
